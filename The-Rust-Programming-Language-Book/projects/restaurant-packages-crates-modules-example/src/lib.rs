8a659ab0971196c380b7bc5d01e850d0311df070
HrushiBorhade <hrushiborhade123@gmail.com>
2025-01-23 16:45:14 +0530

    Add struct breakfast in restaurant example

--- The-Rust-Programming-Language-Book/projects/restaurant-packages-crates-modules-example/src/lib.rs
+++ The-Rust-Programming-Language-Book/projects/restaurant-packages-crates-modules-example/src/lib.rs
@@ -1,9 +1,10 @@
+fn random_function() {}
+
 mod front_of_house {
     pub mod hosting {
         pub fn add_to_waitlist() {
             println!("you're added to the waitlist, Thank you for you patience");
         }
-
         fn seat_at_table() {}
     }
 
@@ -16,10 +17,32 @@ mod front_of_house {
     }
 }
 
+mod back_of_house {
+    #[derive(Debug)]
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit : String
+    }
+
+    impl Breakfast {
+        pub fn summer(toast :&str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches")
+            }
+        }
+    }
+
+}
+
+
 pub fn eat_at_restaurant() {
     // absolute path
     crate::front_of_house::hosting::add_to_waitlist();
 
     // relative path 
     front_of_house::hosting::add_to_waitlist();
+    let mut  breakfast = back_of_house::Breakfast::summer("Wheat");
+    breakfast.toast = String::from("Rye");
+    println!("breakfast menu: {breakfast:?}");
 }
