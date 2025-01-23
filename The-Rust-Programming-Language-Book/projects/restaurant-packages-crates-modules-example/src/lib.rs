ba5e74ad74c753b8ed0f5cb43badb823ea94a881
HrushiBorhade <hrushiborhade123@gmail.com>
2025-01-23 13:36:07 +0530

    Add basic path in restaurant example

--- The-Rust-Programming-Language-Book/projects/restaurant-packages-crates-modules-example/src/lib.rs
+++ The-Rust-Programming-Language-Book/projects/restaurant-packages-crates-modules-example/src/lib.rs
@@ -1,6 +1,8 @@
 mod front_of_house {
-    mod hosting {
-        fn add_to_waitlist() {}
+    pub mod hosting {
+        pub fn add_to_waitlist() {
+            println!("you're added to the waitlist, Thank you for you patience");
+        }
 
         fn seat_at_table() {}
     }
@@ -12,4 +14,12 @@ mod front_of_house {
 
         fn take_payment() {}
     }
+}
+
+pub fn eat_at_restaurant() {
+    // absolute path
+    crate::front_of_house::hosting::add_to_waitlist();
+
+    // relative path 
+    front_of_house::hosting::add_to_waitlist();
 }
