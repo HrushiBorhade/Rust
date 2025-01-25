c72a64f4356622df3bdeaea78f47bb77aa6eece7
HrushiBorhade <hrushiborhade123@gmail.com>
2025-01-25 09:52:14 +0530

    Add hashmap to collection example

--- The-Rust-Programming-Language-Book/projects/collections/src/main.rs
+++ The-Rust-Programming-Language-Book/projects/collections/src/main.rs
@@ -1,5 +1,15 @@
 mod vector;
+use std::collections::HashMap;
 
 fn main() {
     vector::demonstrate_vector_operations();
+
+    let mut scores = HashMap::new();
+
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
+
+    let team_name = String::from("Blue");
+    let score = scores.get(&team_name).copied().unwrap_or(0);
+
 }
