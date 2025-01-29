e2f12356310818a650beea7e9062dcb1bd18ebc3
HrushiBorhade <hrushiborhade123@gmail.com>
2025-01-29 19:52:52 +0530

    Add hashMap operations to collection

---
+++ The-Rust-Programming-Language-Book/projects/collections/src/hashmap_example.rs
@@ -0,0 +1,42 @@
+use std::collections::HashMap;
+
+pub fn demonstarte_hashmap_operation() {
+    let mut scores = HashMap::new();
+
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
+
+    let team_name = String::from("Blue");
+    //  The get method returns an Option<&V>; if there’s no value for that key in the hash map, get will return None. This program handles the Option by calling copied to get an Option<i32> rather than an Option<&i32>, then unwrap_or to set score to zero if scores doesn’t have an entry for the key.
+    let score = scores.get(&team_name).copied().unwrap_or(0);
+
+    println!("score for blue team: {} ", team_name);
+
+    for (key, value) in &scores {
+        println!("{key}: {value}");
+    }
+
+    let mut scores = HashMap::new();
+
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Blue"), 25);
+
+    println!("{scores:?}");
+
+    let mut scores = HashMap::new();
+    scores.insert(String::from("Blue"), 10);
+
+    scores.entry(String::from("Yellow")).or_insert(50);
+    scores.entry(String::from("Blue")).or_insert(50);
+
+    println!("{scores:?}");
+
+    let text = "hello world wonderful world";
+
+    let mut map = HashMap::new();
+
+    for word in text.split_whitespace() {
+        let count = map.entry(word).or_insert(0);
+        *count += 1;
+    }
+}
--- The-Rust-Programming-Language-Book/projects/collections/src/main.rs
+++ The-Rust-Programming-Language-Book/projects/collections/src/main.rs
@@ -1,16 +1,9 @@
 mod vector;
 mod string;
-use std::collections::HashMap;
+mod hashmap_example;
 
 fn main() {
     vector::demonstrate_vector_operations();
     string::demonstrate_string_operations();
-    let mut scores = HashMap::new();
-
-    scores.insert(String::from("Blue"), 10);
-    scores.insert(String::from("Yellow"), 50);
-
-    let team_name = String::from("Blue");
-    let score = scores.get(&team_name).copied().unwrap_or(0);
-
+    hashmap_example::demonstarte_hashmap_operation();
 }
