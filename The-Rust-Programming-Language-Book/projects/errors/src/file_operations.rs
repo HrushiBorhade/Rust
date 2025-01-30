787b7fcf38bff822e55521a3bb2fbad8abc06f10
HrushiBorhade <hrushiborhade123@gmail.com>
2025-01-30 13:49:16 +0530

    Add fs::read_to_string() to propogate example

--- The-Rust-Programming-Language-Book/projects/errors/src/file_operations.rs
+++ The-Rust-Programming-Language-Book/projects/errors/src/file_operations.rs
@@ -1,5 +1,5 @@
-use std::fs::File;
-use std::io::{self, Read};
+use std::fs;
+use std::io;
 
 pub fn read_username_from_file() -> Result<String, io::Error> {
 //    let username_file_result = File::open("hello.txt");
@@ -16,8 +16,10 @@ pub fn read_username_from_file() -> Result<String, io::Error> {
         //Err(e) => Err(e),
 //    }
 
-    let mut username = String::new();
-    File::open("hello.txt")?.read_to_string(&mut username)?;
-    Ok(username)
+//    let mut username = String::new();
+//    File::open("hello.txt")?.read_to_string(&mut username)?;
+//    Ok(username)
+
+    fs::read_to_string("hello.txt");
 }
 
