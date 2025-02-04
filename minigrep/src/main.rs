9b468661b1471e9955ccb146fb893aea72881fd2
HrushiBorhade <hrushiborhade123@gmail.com>
2025-02-04 19:02:19 +0530

    feat: modular config code

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -4,13 +4,27 @@ use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let file_path = &args[2];
+    let config = Config::new(&args);
 
-    println!("Searching for {query}");
-    println!("In file {file_path}");
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
 
-    let file_content = fs::read_to_string(file_path).expect("Error reading content of the file");
+    let file_content =
+        fs::read_to_string(config.file_path).expect("Error reading content of the file");
 
     println!("File Content:\n{file_content}");
+}
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query: String = args[1].clone();
+        let file_path = args[2].clone();
+
+        Config { query, file_path }
+    }
 }
