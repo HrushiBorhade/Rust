3f9c4b24f31a65c6b91fb41f09a7bd9b395fc386
HrushiBorhade <hrushiborhade123@gmail.com>
2025-02-08 14:20:23 +0530

    Add Rc<T> example to smart pointers

--- The-Rust-Programming-Language-Book/projects/smart-pointers/src/main.rs
+++ The-Rust-Programming-Language-Book/projects/smart-pointers/src/main.rs
@@ -1,17 +1,19 @@
 use crate::List::{Cons, Nil};
-use std::ops::Deref;
 use std::mem::drop;
+use std::ops::Deref;
+use std::rc::Rc;
+use std::sync::Condvar;
 
 #[derive(Debug)]
 enum List<T> {
-    Cons(T, Box<List<T>>),
+    Cons(T, Rc<List<T>>),
     Nil,
 }
 
 struct MyBox<T>(T);
 
 impl<T> MyBox<T> {
-    fn new(x:T) -> MyBox<T>{
+    fn new(x: T) -> MyBox<T> {
         MyBox(x)
     }
 }
@@ -26,7 +28,7 @@ impl<T> Deref for MyBox<T> {
 
 #[derive(Debug)]
 struct CustomSmartPointer {
-    data: String
+    data: String,
 }
 
 impl Drop for CustomSmartPointer {
@@ -36,13 +38,11 @@ impl Drop for CustomSmartPointer {
 }
 
 fn main() {
-    let b = Box::new(5);
-    println!("b = {b}");
+    // let b = Box::new(5);
+    // println!("b = {b}");
 
-    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
-    println!("list : {:#?}", list);
-
-
+    // let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+    // println!("list : {:#?}", list);
 
     // Deref trait
     let x = 5;
@@ -50,7 +50,7 @@ fn main() {
 
     println!("y : {}", *y);
 
-    let y  = MyBox::new(x);
+    let y = MyBox::new(x);
     println!("y for MyBox: {}", *y);
 
     //Deref Coercion
@@ -59,17 +59,26 @@ fn main() {
 
     // Drop trait - automatically calls after it is out of scope
     let c = CustomSmartPointer {
-        data: String::from("random shit")
+        data: String::from("random shit"),
     };
     let d = CustomSmartPointer {
-        data: String::from("random shit 2")
+        data: String::from("random shit 2"),
     };
 
     println!("CustomSmartPointers c and d created");
     drop(d);
     println!("d is now dropped before getting out of scope");
-    println!("Out of scope")
 
+    let a1 = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    println!("count after creating a1 = {}", Rc::strong_count(&a1));
+    let a2 = Cons(3, Rc::clone(&a1));
+    println!("count after creating a1 = {}", Rc::strong_count(&a1));
+    {
+        let a3 = Cons(4, Rc::clone(&a1));
+        println!("count after creating a3 = {}", Rc::strong_count(&a1));
+    }
+    println!("count after a3 goes out of scope = {}", Rc::strong_count(&a1));
+    println!("Out of scope");
 }
 
 fn hello(name: &str) {
