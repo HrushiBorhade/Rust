5f371b9a5a04445331c5442f6b9ed8edf00c419a
HrushiBorhade <hrushiborhade123@gmail.com>
2025-02-08 23:53:52 +0530

    Add join and move to thread example

--- The-Rust-Programming-Language-Book/projects/threads/src/main.rs
+++ The-Rust-Programming-Language-Book/projects/threads/src/main.rs
@@ -1,17 +1,25 @@
 use std::thread;
 use std::time::Duration;
+
 fn main() {
-    thread::spawn(|| {
+    let handle = thread::spawn(|| {
         for i in 1..10 {
-            println!("hi number {i} from spawned thread");
+            println!("hi number {i} from the spawned thread!");
             thread::sleep(Duration::from_millis(1));
         }
     });
 
-    println!("Hello from main thread");
-
     for i in 1..5 {
-        println!("hi number {i} from main thread");
+        println!("hi number {i} from the main thread!");
         thread::sleep(Duration::from_millis(1));
     }
+
+    let v = vec![1, 2, 3];
+
+    let handle2 = thread::spawn(move || {
+        println!("Here's a vector: {v:?}");
+    });
+
+    handle.join().unwrap();
+    handle2.join().unwrap();
 }
