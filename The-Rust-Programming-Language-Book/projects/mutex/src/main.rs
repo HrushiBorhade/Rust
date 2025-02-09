ea3a6eb3867abca4afbb11bc93c7276a67edd509
HrushiBorhade <hrushiborhade123@gmail.com>
2025-02-09 11:03:07 +0530

    Add basic mutex example

---
+++ The-Rust-Programming-Language-Book/projects/mutex/src/main.rs
@@ -0,0 +1,13 @@
+use std::sync::Mutex;
+
+
+fn main() {
+    let m = Mutex::new(5);
+
+    {
+        let mut num = m.lock().unwrap();
+        *num = 6;
+    }
+
+    println!("value of m: {:#?}", m)
+}
