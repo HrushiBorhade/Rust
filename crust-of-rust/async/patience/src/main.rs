6309527e1feae46b1f288b243ed92a136267dcc7
HrushiBorhade <hrushiborhade123@gmail.com>
2025-02-14 15:35:20 +0530

    COR:async - logs to understand async flow

--- crust-of-rust/async/patience/src/main.rs
+++ crust-of-rust/async/patience/src/main.rs
@@ -9,17 +9,22 @@ use tokio::time::{sleep, Duration};
 async fn main() {
     println!("hello from async world");
     greet().await;
-
+    println!("before task execution");
     let handle1 = tokio::spawn(task_one());
     let handle2 = tokio::spawn(task_two());
     let handle3 = tokio::spawn(task_three());
     handle1.await;
+    println!("between task execution 1 and 2");
     handle2.await;
+    println!("between task execution 2 and 3");
     handle3.await;
-
+    println!("after task execution before get api response");
     get_api_response().await;
+    println!("between get api and read content");
     read_content().await;
+    println!("between read content and channel communication");
     channel_comm().await;
+    println!("end of the code");
 }
 
 async fn get_api_response() -> Result<(), Box<dyn Error>> {
