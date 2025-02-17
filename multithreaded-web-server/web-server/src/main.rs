bac1ac4c162429320234f642c5ee76479293bbc6
HrushiBorhade <hrushiborhade123@gmail.com>
2025-02-17 09:57:34 +0530

    feat: add response to http request

--- multithreaded-web-server/web-server/src/main.rs
+++ multithreaded-web-server/web-server/src/main.rs
@@ -18,5 +18,8 @@ fn handle_connection(mut stream: TcpStream) {
         .map(|res| res.unwrap())
         .take_while(|line| !line.is_empty())
         .collect();
-    println!("Request: {http_req:#?}");
+    println!("HTTP request : {http_req:#?}");
+    let response = "HTTP/1.1 200 OK\r\n\r\n";
+
+    stream.write_all(response.as_bytes()).unwrap();
 }
