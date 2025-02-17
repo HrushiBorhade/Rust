41356174a4ccb48c28c596ed46eb1476725f366a
HrushiBorhade <hrushiborhade123@gmail.com>
2025-02-17 15:51:46 +0530

    feat: simulating a slow request

--- multithreaded-web-server/web-server/src/main.rs
+++ multithreaded-web-server/web-server/src/main.rs
@@ -2,6 +2,8 @@ use std::{
     fs,
     io::{prelude::*, BufReader, Result},
     net::{TcpListener, TcpStream},
+    thread,
+    time::Duration,
 };
 
 fn main() {
@@ -43,10 +45,13 @@ fn handle_connection(mut stream: TcpStream) -> Result<()> {
         }
     };
 
-    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
-        ("HTTP/1.1 200 OK", "hello.html")
-    } else {
-        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    let (status_line, filename) = match &request_line[..] {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", "hello.html")
+        }
+        _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
     };
 
     let contents =
