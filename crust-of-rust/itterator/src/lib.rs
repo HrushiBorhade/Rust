78e9a1b0958995a127ba764d0b2f64d96a5569ed
HrushiBorhade <hrushiborhade123@gmail.com>
2025-02-19 19:23:47 +0530

    feat: Itterator custom flatten function v1.0

---
+++ crust-of-rust/itterator/src/lib.rs
@@ -0,0 +1,39 @@
+pub fn flatten<I>(iter: I) -> Flatten<I> {
+    Flatten::new(iter)
+}
+
+pub struct Flatten<O> {
+    outer: O,
+}
+
+impl<O> Flatten<O> {
+    pub fn new(iter: O) -> Self {
+        Flatten { outer: iter }
+    }
+}
+
+impl<O> Iterator for Flatten<O>
+where
+    O: Iterator,
+    O::Item: IntoIterator,
+{
+    type Item = <O::Item as IntoIterator>::Item;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.outer.next().and_then(|inner| inner.into_iter().next())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn empty() {
+        assert_eq!(flatten(std::iter::empty::<Vec<()>>()).count(), 0)
+    }
+    #[test]
+    fn one() {
+        assert_eq!(flatten(std::iter::once(vec!["a"])).count(), 1)
+    }
+}
---
+++ crust-of-rust/itterator/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let vs = vec![1, 2, 3, 4];
+
+    for v in vs.iter() {
+        println!("{v}");
+    }
+    println!("{:?}", vs);
+}
